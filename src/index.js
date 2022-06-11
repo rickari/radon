3c1e0d7ec554a7c6a7513bce2e0123aadd1322c5
rickari <aritra25061994@gmail.com>
2022-06-12 00:00:03 +0530

    hello brother

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://rick2506:aritra%402014@cluster0.xv9tb.mongodb.net/Aritra2506", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
